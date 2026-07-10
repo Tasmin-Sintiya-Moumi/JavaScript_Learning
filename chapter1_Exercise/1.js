c1a1323de93a4a2f5872a3806a967b1fe5803934
Tasmin-Sintiya-Moumi <tasminsintiyamoumi@gmail.com>
2026-07-10 21:34:19 +0600

    solved

---
+++ chapter1_Exercise/1.js
@@ -0,0 +1,5 @@
+//Print your name, your department, and your university on three separate lines.
+
+console.log("Tasmin Sintiya Moumi");
+console.log("CSE");
+console.log("Metropolitan University");
