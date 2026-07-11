46d886e75580d047d31df1dc8d5a9f9684ccac61
Tasmin-Sintiya-Moumi <tasminsintiyamoumi@gmail.com>
2026-07-11 11:52:10 +0600

    solved

--- chapter1_Exercise/3.js
+++ chapter1_Exercise/3.js
@@ -1,4 +1,4 @@
 //Deliberately remove a quotation mark from a console.log line, run it, and read the error message carefully.
 //Getting comfortable with errors starts now.
 
-console.log(Hello World);
+//console.log(Hello World);
---
+++ chapter3_Exercise/1.js
@@ -0,0 +1,16 @@
+// Write a program that prints whether a number is positive, negative, or zero. 
+
+let a= 7;
+
+if(a>0)
+{
+    console.log("The number is positive");
+}
+else if(a<0)
+{
+    console.log("The number is negative");
+}
+else if(a==0)
+{
+    console.log("The number is zero");
+}
