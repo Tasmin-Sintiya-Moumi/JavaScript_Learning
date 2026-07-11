5e97b46a532c7dec3d6ce91316ebd8dc7def427e
Tasmin-Sintiya-Moumi <tasminsintiyamoumi@gmail.com>
2026-07-11 19:26:04 +0600

    done

---
+++ chapter5_Exercise/1.js
@@ -0,0 +1,6 @@
+//Write a function isEven(n) that returns true or false, then use it inside a loop from 1 to 10.
+
+function isEven(n)
+{
+    
+}
---
+++ chapter5_Exercise/2.js
@@ -0,0 +1,10 @@
+// Write celsiusToFahrenheit(c) using the formula F = C × 9/5 + 32. 
+
+
+function celciusToFarenheit(C)
+{
+    let F = ((C * 9)/5) +32;
+    return F;
+}
+
+console.log(celciusToFarenheit(34));
