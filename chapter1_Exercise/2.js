608cf48316f86b8295e71fd954287cf6d5958900
Tasmin-Sintiya-Moumi <tasminsintiyamoumi@gmail.com>
2026-07-11 09:56:46 +0600

    comments are not printed

---
+++ chapter1_Exercise/2.js
@@ -0,0 +1,8 @@
+//Write a program with a comment on top describing what it does, then print any sentence.
+
+//Single line comment.
+/*Multiple line comment.
+Comments are not printed. Comments are used for many reasons. such as: to explain,
+to make it easier for programmers to understand,debug,maintain etc.*/
+
+console.log("No comments");
