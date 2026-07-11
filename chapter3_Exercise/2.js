14f09e3ae5acbef282051f7e550702b4a9329cbb
Tasmin-Sintiya-Moumi <tasminsintiyamoumi@gmail.com>
2026-07-11 11:56:38 +0600

    solved

---
+++ chapter3_Exercise/2.js
@@ -0,0 +1,16 @@
+// Given a temperature variable, print 'Hot' above 32, 'Pleasant' between 20 and 32, otherwise 'Cold'. 
+
+let a= 35;
+
+if(a>32)
+{
+    console.log("Hot");
+}
+else if(a>=20 && a<=32)
+{
+    console.log("Pleasant");
+}
+else
+{
+    console.log("Cold");
+}
