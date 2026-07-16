3ab3de915857777e79b302995f0dc1f9a48bd5a7
Tasmin-Sintiya-Moumi <tasminsintiyamoumi@gmail.com>
2026-07-16 22:30:58 +0600

    done

--- chapter5_Exercise/4.js
+++ chapter5_Exercise/4.js
@@ -1,1 +1,22 @@
 //Rewrite your FizzBuzz from Chapter 4 as a function fizzbuzz(limit).
+
+function fizzbuzz(limit)
+{
+    for (let i=1; i<=limit; i++)
+    {
+        if(i%3==0 && i%5==0)
+        {
+            console.log("FizzBuzz");
+        }
+        else if(i%3==0)
+        {
+            console.log("Fizz");
+        }
+        else if(i%5==0)
+        {
+            console.log("Buzz");
+        }
+    }
+}
+
+(fizzbuzz(30));
