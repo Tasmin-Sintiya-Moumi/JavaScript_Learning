961828fb5ccc0e173bb009e84b327abe37f9f024
Tasmin-Sintiya-Moumi <tasminsintiyamoumi@gmail.com>
2026-07-30 14:00:28 +0600

    done

---
+++ chapter7_Exercise/1.js
@@ -0,0 +1,8 @@
+const myself ={
+    name:"Moumi",
+    dept:"CSE",
+    semester:"4th year 1st semester",
+    skills:["Machine Learning","Arts and crafts","Photography","Digital Illustration"]
+};
+
+console.log(`My name is ${myself.name}, a student of the ${myself.dept} department, currently in ${myself.semester}, and my skills are: ${myself.skills.join(", ")}`)
